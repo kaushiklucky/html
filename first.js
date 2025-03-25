dd19b22d0c782d707d270d3c2768d4d793c75bb0
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-03-25 10:49:11 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,28 @@
+var num = 34;
+var num2 = 23;
+console.log(num+num2);
+
+var arr = [1,2,3,4,5,6,"lucky", 'kaushik', null, true]
+console.log(arr)
+
+var obj = {
+    name: 'kl',
+    roll: 3,
+    school: 'gbsss',
+    address: 'mohan garden'
+}
+
+function fun(a, b) {
+    console.log(a+b)
+}
+
+fun(5,6);
+fun(8,9);
+fun(6,8);
+fun(4,6);
+
+function gun(a, b) {
+    return a*b
+}
+var c = gun(6,4);
+console.log(c);
